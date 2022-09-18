a5d57786b6b1c4c44c4a33dc40ca49621a441d4e
Steven Tran <steven29160@gmail.com>
2022-09-17 21:32:22 -0400

    Interface for the api classes

---
+++ multiplaylist/src/main/java/com/transteven/multiplaylist/content/Content.java
@@ -0,0 +1,6 @@
+package com.transteven.multiplaylist.content;
+
+    public interface Content {
+        public void getInstancesAmount(int audioAmount);
+        public void getPlaylistId(String playlistId);
+}
