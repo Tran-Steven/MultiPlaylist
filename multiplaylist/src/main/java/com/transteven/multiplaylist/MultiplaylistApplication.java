791bc3dabeafd48973bf439687ed43769db5cbf3
Steven Tran <steven29160@gmail.com>
2022-09-17 21:32:52 -0400

    Fresh Maven download & Classes for API

---
+++ multiplaylist/src/main/java/com/transteven/multiplaylist/MultiplaylistApplication.java
@@ -0,0 +1,13 @@
+package com.transteven.multiplaylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiplaylistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiplaylistApplication.class, args);
+	}
+
+}
---
+++ multiplaylist/src/test/java/com/transteven/multiplaylist/MultiplaylistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transteven.multiplaylist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiplaylistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
