4071a40ee86f8d0240c50ad946d93e1596a6828d
Steven Tran <steven29160@gmail.com>
2022-09-19 00:56:40 -0400

    d

--- multiplaylist/src/main/java/com/transteven/multiplaylist/MultiplaylistApplication.java
+++ multiplaylist/src/main/java/com/transteven/multiplaylist/MultiplaylistApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MultiplaylistApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(MultiplaylistApplication.class, args);
 	}
+
 }
