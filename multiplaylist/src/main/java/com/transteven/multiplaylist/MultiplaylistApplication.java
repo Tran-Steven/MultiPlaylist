2ad1c7850dac3bc55d191732b485540e2adda99d
Steven Tran <steven29160@gmail.com>
2022-09-20 16:23:28 -0400

    Removed comments that were breaking code

--- multiplaylist/src/main/java/com/transteven/multiplaylist/MultiplaylistApplication.java
+++ multiplaylist/src/main/java/com/transteven/multiplaylist/MultiplaylistApplication.java
@@ -5,13 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MultiplaylistApplication {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> c9d6b63 (Fresh Maven download & Classes for API)
-=======
->>>>>>> f0052b3 (changed to secrets)
 	public static void main(String[] args) {
 		SpringApplication.run(MultiplaylistApplication.class, args);
 	}
