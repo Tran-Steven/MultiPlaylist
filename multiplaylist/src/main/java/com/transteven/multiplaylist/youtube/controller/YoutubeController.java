2c1a2a43380ba606bfd7987d853ba9778f5bbc17
Steven Tran <steven29160@gmail.com>
2022-09-27 12:52:49 -0400

    RequestMethod to post,get,put,delete mapping

--- multiplaylist/src/main/java/com/transteven/multiplaylist/youtube/controller/YoutubeController.java
+++ multiplaylist/src/main/java/com/transteven/multiplaylist/youtube/controller/YoutubeController.java
@@ -7,6 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.transteven.multiplaylist.youtube.model.YoutubeModel;
@@ -20,25 +24,25 @@ public class YoutubeController {
 	@Autowired
         YoutubeService ytService;
 
-		@RequestMapping(value="/playlist/yt", method=RequestMethod.POST)
+		@PostMapping(value="/playlist/yt")
 		public YoutubeModel createYoutube(@RequestBody YoutubeModel yt) {
 			return ytService.createYoutube(yt);
 		}
 
 
-		@RequestMapping(value="/playlist/yt", method=RequestMethod.GET)
+		@GetMapping(value="/playlist/yt")
 			public List<YoutubeModel> readYoutube() {
     		return ytService.getYoutube();
 		}
 
 
-		@RequestMapping(value="/playlist/yt/{ytId}", method=RequestMethod.PUT)
+		@PutMapping(value="/playlist/yt/{ytId}")
 			public YoutubeModel readYoutube(@PathVariable(value = "ytId") Long id, @RequestBody YoutubeModel ytDetails) {
     		return ytService.updateYoutube(id, ytDetails);
 		}
 
 
-		@RequestMapping(value="/playlist/yt/{ytId}", method=RequestMethod.DELETE)
+		@DeleteMapping(value="/playlist/yt/{ytId}")
 			public void deleteYoutube(@PathVariable(value = "ytId") Long id) {
 		    ytService.deleteYoutube(id);
 		}
