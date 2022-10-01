ae30725328e248251d0fdb7455524ca2b2bc6def
Steven Tran <steven29160@gmail.com>
2022-10-01 13:30:06 -0400

    Changed to PlaylistService instead of Playlist

---
+++ multiplaylist/client/src/components/Playlist/PlaylistService.ts
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from "react";
+import http from "../../utils/http-common";
+
+const PlaylistService = (playlistUrl) => {
+  const [playlistId, setPlaylistId] = useState<String>();
+
+
+  
+};
